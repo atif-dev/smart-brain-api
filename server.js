1d6d05db41f58afb549d173179d673b64429dba6
atif-dev <ati1254@gmail.com>
2022-05-10 18:27:58 +0500

    server.js/db==>db==>connection==>added connectionString

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const users = require('./controllers/users');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl: {
     rejectUnauthorized: false
     }
